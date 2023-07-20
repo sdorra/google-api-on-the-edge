3fec6453403e8e75c7c886926a23fd52a4d45f57
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2023-07-20 20:40:47 +0200

    Add on-update route

---
+++ app/on-update/route.ts
@@ -0,0 +1,19 @@
+import { env } from "@/lib/env.mjs";
+import { revalidatePath } from "next/cache";
+
+export async function POST(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const token = searchParams.get("token");
+
+  if (token !== env.ON_UPDATE_TOKEN) {
+    return new Response("Unauthorized", {
+      status: 401,
+    });
+  }
+
+  revalidatePath("/isr");
+
+  return new Response("OK", {
+    status: 200,
+  });
+}
