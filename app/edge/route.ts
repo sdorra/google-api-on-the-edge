d7d27c5d7e643b53fe05cb52538148b3d1db945a
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2023-07-20 15:10:42 +0200

    Implement serverless and edge function

--- app/edge/route.ts
+++ app/edge/route.ts
@@ -1,5 +1,53 @@
+import { SignJWT, importPKCS8 } from "jose";
+import { env } from "@/lib/env.mjs";
+
 export const runtime = "edge";
 
+const apiUrl = `https://sheets.googleapis.com/v4/spreadsheets/${env.SPREADSHEET_ID}/values/A2`;
+
+const payload = {
+  iss: env.GOOGLE_SA_CLIENT_EMAIL,
+  scope: "https://www.googleapis.com/auth/spreadsheets.readonly",
+  aud: "https://www.googleapis.com/oauth2/v4/token",
+  exp: Math.floor(Date.now() / 1000) + 60 * 60, // 1 hour expiration
+  iat: Math.floor(Date.now() / 1000),
+};
+
 export async function GET() {
-  return new Response("Hello from the edge function!");
+  const privateKey = await importPKCS8(env.GOOGLE_SA_PRIVATE_KEY, "RS256");
+
+  const token = await new SignJWT(payload)
+    .setProtectedHeader({ alg: "RS256" })
+    .setIssuedAt()
+    .setIssuer(env.GOOGLE_SA_CLIENT_EMAIL)
+    .setAudience("https://www.googleapis.com/oauth2/v4/token")
+    .setExpirationTime("1h")
+    .sign(privateKey);
+
+  // Form data for the token request
+  const form = {
+    grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
+    assertion: token,
+  };
+
+  // Make the token request
+  const tokenResponse = await fetch(
+    "https://www.googleapis.com/oauth2/v4/token",
+    {
+      method: "POST",
+      body: JSON.stringify(form),
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+
+  const tokenData = await tokenResponse.json();
+  // Use the access token to make the API request
+  const apiResponse = await fetch(apiUrl, {
+    headers: { Authorization: `Bearer ${tokenData.access_token}` },
+  });
+  const data = await apiResponse.json();
+
+  return new Response(data.values[0][0], {
+    status: 200,
+  });
 }
--- app/serverless/route.ts
+++ app/serverless/route.ts
@@ -1,3 +1,29 @@
+import { google } from "googleapis";
+import { env } from "@/lib/env.mjs";
+
+const sheets = google.sheets("v4");
+
+const jwtClient = new google.auth.JWT(
+  env.GOOGLE_SA_CLIENT_EMAIL,
+  undefined,
+  env.GOOGLE_SA_PRIVATE_KEY,
+  ["https://www.googleapis.com/auth/spreadsheets"]
+);
+
 export async function GET() {
-  return new Response("Hello from the serverless function!");
+  await jwtClient.authorize();
+
+  const data = await sheets.spreadsheets.values.get({
+    auth: jwtClient,
+    spreadsheetId: env.SPREADSHEET_ID,
+    range: "A1",
+  });
+
+  if (!data.data.values) {
+    throw new Error("sheet has no data");
+  }
+
+  return new Response(data.data.values[0][0], {
+    status: 200,
+  });
 }
