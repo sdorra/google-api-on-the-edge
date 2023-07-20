bb168d33853c8a09d2783f67dd8ac5dfaa9dbd75
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2023-07-20 20:10:44 +0200

    force cache on isr route

--- app/isr/route.ts
+++ app/isr/route.ts
@@ -2,6 +2,7 @@ import fetchCell from "@/lib/fetchCell";
 
 export const runtime = "edge";
 export const revalidate = false;
+export const fetchCache = "force-cache";
 
 export async function GET() {
   const data = await fetchCell("A3");
